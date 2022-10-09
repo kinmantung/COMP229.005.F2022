2ee9ce852ceda77d3ae711e4798fe87bf45aa71c
Kin Man Tung <tyronetung1206@gmail.com>
2022-10-08 22:26:33 -0400

    Assignment1_301216721

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Student ID: 301216721
 // Name: Kin Man Tung
-// Date
+// Date: 8/10/2022
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const { toUSVString } = require('util')
 
 var app = express();
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,42 @@
+ /* Student ID: 301216721
+ Name: Kin Man Tung
+ Date: 8/10/2022 */
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', name:'Julio' });
+  res.render('home', { title: 'Home' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('index',
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme',
    {
-     title: 'About Me', 
-     name: 'Julio'
+     title: 'About Me'
     }
     );
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('index',
+  res.render('projects',
    {
-     title: 'Projects', 
-     name: 'Julio'
+     title: 'Projects'
     }
     );
 });
 
-router.get('/contact', function(req, res, next) {
-  res.render('index',
+router.get('/contactme', function(req, res, next) {
+  res.render('contactme',
    {
-     title: 'Contact', 
-     name: 'Julio'
+     title: 'Contact Me'
     }
     );
 });
 
 router.get('/services', function(req, res, next) {
-  res.render('projectsservices',
+  res.render('services',
    {
-     title: 'Services', 
-     name: 'Julio'
+     title: 'Services'
     }
     );
 });
