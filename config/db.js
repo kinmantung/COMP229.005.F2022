705bf2cae9ed6751d789c4c97a03d116061fc589
Kin Man Tung <tyronetung1206@gmail.com>
2022-10-21 15:23:39 -0400

    Week05_Final

---
+++ config/db.js
@@ -0,0 +1,18 @@
+// In real project, never expose your credentail in your code.
+let atlasDB ="mongodb+srv://dbuser:hZc110oKk3Cd8vcj@cluster005.u92wuqx.mongodb.net/products?retryWrites=true&w=majority";
+
+let mongoose = require('mongoose');
+
+module.exports = function(){
+
+    // Conect to the database
+    mongoose.connect(atlasDB);
+
+    let mongodb = mongoose.connection;
+    mongodb.on('error', console.error.bind(console, 'Connection Error:'));
+    mongodb.once('open', ()=>{
+        console.log('==== Conected to MongoDB ====');
+    });
+
+    return mongodb;
+}
